0d32c8641c6d83cfad95838298a0c24a0dddf145
Vitoria Moreira Pires <vitoriapquenatalento@gmail.com>
2026-02-23 08:46:10 -0300

    Add files via upload

---
+++ JS_aula01_230226/variaveis.js
@@ -0,0 +1,33 @@
+var  nomeVariavel = "valor da variável";
+var outraVariavel = "Valor de outra variável";
+var variavelIdade = 22;
+var variavelTenhoCabeloBranco = false;
+var variavelIndefinida;
+
+console.log(nomeVariavel);
+console.log(outraVariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
+console.log(variavelIndefinida);
+// console.log(variavelNula);
+
+// Dados Bolanos
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+// Neste caso tr irá conter o valor true e fl o valor false.
+// A variável int1 irá conter o valor 2 (true + 1), pois true é
+// automaticamente convertido para 1 e int2 irá conter o valor 1
+// (false + 1), pois false é convertido para 0.
+
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(f1);
+console.log(int1);
+console.log(int2);
