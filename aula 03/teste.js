c82c59e73a5047a3548d62ac1dd604cf3a9919b6
Vitoria Moreira Pires <vitoriapquenatalento@gmail.com>
2026-03-02 08:46:47 -0300

    Add files via upload

---
+++ aula 03/teste.js
@@ -0,0 +1,9 @@
+/* let resposta = confirm("Você deseja continuar?");
+if (resposta) {
+alert("Você escolheu continuar.");
+} else {
+alert("Você cancelou a ação.");
+} */
+
+let nome = prompt("Qual é o seu nome?");
+alert("Olá, " + nome + "!");
