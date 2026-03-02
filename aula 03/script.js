b453f4125561eeea8601619784380bf86332108a
Vitoria Moreira Pires <vitoriapquenatalento@gmail.com>
2026-03-02 08:10:20 -0300

    Add files via upload

---
+++ aula 03/script.js
@@ -0,0 +1,53 @@
+/* // a)
+let A = 10;
+let B = 20;
+
+console.log(B);
+
+B = 5;
+
+console.log(A, B); */
+
+// b)
+/* let A = 30;
+let B = 20;
+let C = A + B;
+
+console.log(C);
+
+B = 10;
+
+console.log(C);
+
+C = A + B;
+
+console.log(A, B, C); */
+
+// c)
+/* let A = 10;
+let B = 20;
+let C = A;
+
+A = B;
+
+console.log(A, B, C); */
+// d)
+/* let A = 10;
+let B = A + 1;
+
+A = B + 1;
+B = A;
+
+console.log(A);
+
+A = B + 1;
+
+console.log(B); */
+
+let nota1 = 5;
+let nota2 = 10;
+let nota3 = 8;
+
+var media = (nota1*2 + nota2 * 3 + nota3 * 5) / 10;
+
+console.log(media);
