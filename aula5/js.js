48f32b35078a80f0ce005c6219280ecc63207df4
Vitoria Moreira Pires <vitoriapquenatalento@gmail.com>
2026-03-30 10:49:40 -0300

    Add files via upload

---
+++ aula5/js.js
@@ -0,0 +1,31 @@
+// exercicio a
+
+function numMaior(x, y){
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+// alert(numMaior(3, 4))
+
+
+
+
+
+// exercicio b
+
+function parOuImpar(num) {
+    if (num % 2 === 0){
+        return true;
+    } else {
+        false;
+    }
+}
+// alert(parOuImpar(6))
+
+
+
+
+
+// exercicio 3
